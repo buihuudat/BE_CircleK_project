54711cddde6b61bf308c07875b01249f2cc56796
Bui Huu Dat <dat54261001@gmail.com>
2022-12-02 10:11:51 +0700

    update circle k page

---
+++ CircleKAPI/Models/Order/Order.cs
@@ -0,0 +1,13 @@
+﻿namespace CircleKAPI.Models.Order
+{
+    public class Order
+    {
+        public int id { get; set; }
+        public string UID { get; set; } = string.Empty;
+        public string Voucher { get; set; } = "";
+        public string NameOfUser { get; set; } = string.Empty;
+        public long Phone { get; set; }
+        public string Address { get; set; }
+
+    }
+}
